050882d67d90ed75d949518a95b1ef5208a3094f
Pheoda <n.fondacci@gmail.com>
2017-05-15 10:01:44 +0200

    Avancement partie sendFile

--- src/tp_tftp/Client.java
+++ src/tp_tftp/Client.java
@@ -1,5 +1,7 @@
 package client;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -16,32 +18,102 @@ public class Client {
     public static final byte ERROR = 5;
     
     private static final String IP = "127.0.0.1";
-    private static final int port = 75;
+    InetAddress inetAddress;
+    private static final int port = 69;
+    
+    private DatagramSocket socket;
+    private DatagramPacket packet;
+    private static final int DATA_SIZE = 512;
     
     public static void main(String[] args) throws IOException {
-        DatagramSocket socket = new DatagramSocket();
-        InetAddress inetAddress = InetAddress.getByName(IP);
-        byte [] data = "Hello".getBytes();
-        DatagramPacket packet = new DatagramPacket(data, data.length, inetAddress, port);
-        socket.send(packet);
+        
     }
     
-    private void sendFile(String filename) {
+    private void sendFile(String filename) throws FileNotFoundException, IOException {
+        int ack;
+        byte[] receivedACK;
         // Ouverture fichier local filename
+        FileOutputStream file = new FileOutputStream(filename);
         
-        // Si réussi
         // Créer DatagramSocket
+        socket = new DatagramSocket();
         
-        // Envoyer packet donnée avec WRQ
-        // Recevoir ACK pour confirmation
+        // Envoyer packet WRQ
+        inetAddress = InetAddress.getByName(IP);
+        byte[] data = createRequestPacket(WRQ, filename, "octet");
+        packet = new DatagramPacket(data, data.length, inetAddress, port);
+        socket.send(packet);
+    
+        // Recevoir ACK pour confirmation envoi fichier
+        socket.receive(packet);
+        receivedACK = packet.getData();
+        if(receivedACK[0] == 0 && receivedACK[1] == ACK
+                && receivedACK[2] == 0 && receivedACK[3] == 0) {
+            
+        }
+        else {
+            // On a pas reçu un ACK
+            System.out.println("PAS RECU ACK : " + receivedACK.toString());
+            // Renvoyer packet WRQ ?
+        }
+        
+        // Lecture fichier local (test si < DATA_SIZE)
+        // Envoyer packet DATA
+        // Recevoir packet ACK
         // Refaire ces étapes tant qu'on a des données à envoyer
         
         // Fermeture fichier local
     }
     
-    private void receiveFile(String filename) {
-        // Ouverture fichier filename
+    private void receiveFile(String filename) throws FileNotFoundException {
+        // Création fichier local filename
         
+        // Créer DatagramSocket
+        
+        // Envoyer packet RRQ
+        // Réception packet DATA
+        // Ecriture données dans fichier local
+        // Envoi packet ACK pour confirmation
+        // Refaire tant qu'on a des données à recevoir
+        
+        // Fermeture fichier local
     }
     
+    
+    // Fonction pour créer le packet ACK
+    
+    // Fonction pour créer le packet DATA
+    
+    // Fonction pour créer le packet request RRQ/WRQ
+    private byte[] createRequestPacket(final byte code, final String filename, final String mode) {
+        int tabSize = 2 + filename.length() + 1 + mode.length() + 1;
+        byte[] tab = new byte[tabSize];
+        int cursor = 0;
+        
+        // CODE
+        tab[0] = 0;
+        tab[1] = code;
+        
+        // FILENAME
+        for(int i = 0; i < filename.length(); i++) {
+            tab[i + 2] = (byte)filename.charAt(i);
+            cursor = i + 2;
+        }
+        
+        // ZERO
+        cursor++;
+        tab[cursor] = 0;
+        cursor++;
+        
+        // MODE
+        for(int i = 0; i < mode.length(); i++) {
+            tab[cursor + i] = (byte)mode.charAt(i);
+            cursor++;
+        }
+        
+        // ZERO
+        tab[cursor] = 0;
+        
+        return tab;
+    }
 }
