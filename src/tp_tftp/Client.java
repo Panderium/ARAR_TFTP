7f2abd023bbaf8ce2280698cfaba86db2524b6c9
Nicolas Fondacci <nicolas.fondacci@etu.univ-lyon1.fr>
2017-05-18 17:04:49 +0200

    Rectification fonction createRequest, requête WRQ correcte

--- src/tp_tftp/Client.java
+++ src/tp_tftp/Client.java
@@ -19,18 +19,18 @@ public class Client {
     
     private static final String IP = "127.0.0.1";
     InetAddress inetAddress;
-    private static final int port = 69;
+    private static final int PORT = 69;
     
     private DatagramSocket socket;
     private DatagramPacket packet;
     private static final int DATA_SIZE = 512;
     
     public static void main(String[] args) throws IOException {
-        
+        Client client = new Client();
+        client.sendFile("fichier.txt");
     }
     
     private void sendFile(String filename) throws FileNotFoundException, IOException {
-        int ack;
         byte[] receivedACK;
         // Ouverture fichier local filename
         FileOutputStream file = new FileOutputStream(filename);
@@ -41,7 +41,7 @@ public class Client {
         // Envoyer packet WRQ
         inetAddress = InetAddress.getByName(IP);
         byte[] data = createRequestPacket(WRQ, filename, "octet");
-        packet = new DatagramPacket(data, data.length, inetAddress, port);
+        packet = new DatagramPacket(data, data.length, inetAddress, PORT);
         socket.send(packet);
     
         // Recevoir ACK pour confirmation envoi fichier
@@ -53,7 +53,9 @@ public class Client {
         }
         else {
             // On a pas reçu un ACK
-            System.out.println("PAS RECU ACK : " + receivedACK.toString());
+            System.out.println("PAS RECU ACK : " + receivedACK.length + " caractères.");
+            for(int i = 0; i < receivedACK.length; i++)
+                System.out.println(i + " : " + (int)receivedACK[0]);
             // Renvoyer packet WRQ ?
         }
         
@@ -66,9 +68,11 @@ public class Client {
     }
     
     private void receiveFile(String filename) throws FileNotFoundException {
+        byte[] ack = new byte[2]; // Pour contenir le numero d'ACK
         // Création fichier local filename
         
         // Créer DatagramSocket
+        byte[] data = createRequestPacket(RRQ, filename, "octet");
         
         // Envoyer packet RRQ
         // Réception packet DATA
@@ -89,9 +93,10 @@ public class Client {
         int tabSize = 2 + filename.length() + 1 + mode.length() + 1;
         byte[] tab = new byte[tabSize];
         int cursor = 0;
+        byte zero = 0;
         
         // CODE
-        tab[0] = 0;
+        tab[0] = zero;
         tab[1] = code;
         
         // FILENAME
@@ -99,20 +104,20 @@ public class Client {
             tab[i + 2] = (byte)filename.charAt(i);
             cursor = i + 2;
         }
-        
+
         // ZERO
         cursor++;
-        tab[cursor] = 0;
+        tab[cursor] = zero;
         cursor++;
-        
+    
         // MODE
         for(int i = 0; i < mode.length(); i++) {
-            tab[cursor + i] = (byte)mode.charAt(i);
+            tab[cursor] = (byte)mode.charAt(i);
             cursor++;
         }
         
         // ZERO
-        tab[cursor] = 0;
+        tab[cursor] = zero;
         
         return tab;
     }
